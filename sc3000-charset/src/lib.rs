e1483e913c613b7ac3a186a12d51a507d1d740e4
Ian Tester <imroykun@gmail.com>
2025-10-09 15:41:12 +1100

    sc3000-charset: don't mark the 'Result' type public

--- sc3000-charset/src/lib.rs
+++ sc3000-charset/src/lib.rs
@@ -182,7 +182,7 @@ pub enum ConversionError {
     BufferTooSmall,
 }
 
-pub type Result<T> = core::result::Result<T, ConversionError>;
+type Result<T> = core::result::Result<T, ConversionError>;
 
 /// The character sets that different models of the SC-3000 used
 ///
